84668d69ec9cae114a89410391328559a00a25ab
Andrei Losev <losev_andrei@bk.ru>
2025-05-27 21:34:58 +0300

    init

---
+++ bin/eva_connector.dart
@@ -0,0 +1,5 @@
+import 'package:eva_connector/eva_connector.dart' as eva_connector;
+
+void main(List<String> arguments) {
+  print('Hello world: ${eva_connector.calculate()}!');
+}
---
+++ test/eva_connector_test.dart
@@ -0,0 +1,8 @@
+import 'package:eva_connector/eva_connector.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
