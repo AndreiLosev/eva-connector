3dff9495424865c8bc2b6fd94d59301d5259685a
Andrei Losev <losev_andrei@bk.ru>
2025-06-07 22:33:07 +0300

    all service complited

--- lib/src/eva-config/factory.dart
+++ lib/src/eva-config/factory.dart
@@ -1,12 +1,14 @@
 import 'package:eva_connector/src/eva-config/items/item.dart';
 import 'package:eva_connector/src/eva-config/svcs/base_svc.dart';
 import 'package:eva_connector/src/eva-config/svcs/db_sql/db_sql_history.dart';
+import 'package:eva_connector/src/eva-config/svcs/hmi_service/hmi_service.dart';
 import 'package:eva_connector/src/eva-config/svcs/item_state_expiration_service/item_state_expiration_service.dart';
 import 'package:eva_connector/src/eva-config/svcs/logic_manager/logic_manager.dart';
 import 'package:eva_connector/src/eva-config/svcs/modbus_controller/modbus_controller.dart';
 import 'package:eva_connector/src/eva-config/svcs/mqtt_controller/mqtt_controller.dart';
 import 'package:eva_connector/src/eva-config/svcs/py_macros/py_macros.dart';
 import 'package:eva_connector/src/eva-config/svcs/s7_controller/s7_controller.dart';
+import 'package:eva_connector/src/eva-config/svcs/script_runner_controller/script_runner_controller.dart';
 import 'package:eva_connector/src/eva-config/svcs/shared_lock_service/shared_lock_service.dart';
 import 'package:eva_connector/src/exceptions/unsupported_service.dart';
 
@@ -21,6 +23,8 @@ class Factory {
       SharedLockService.svcCommand => SharedLockService(id),
       ItemStateExpirationService.svcCommand => ItemStateExpirationService(id),
       MqttController.svcCommand => MqttController(id),
+      HmiService.svcCommand => HmiService(id),
+      ScriptRunnerController.svcCommand => ScriptRunnerController(id),
       _ => throw UnsupportedService(map['command']),
     };
 
