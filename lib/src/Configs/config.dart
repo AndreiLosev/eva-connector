c3fa6f481e99837fd5a9f5d2bd8b55e3e56fbda4
Andrei Losev <losev_andrei@bk.ru>
2025-06-11 22:32:07 +0300

    add file api

--- lib/src/Configs/config.dart
+++ lib/src/Configs/config.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:eva_connector/src/Configs/ssh_config.dart';
 
 class Config {
-  final ideName = 'sofkip.ide.${Platform.environment['LOGNAME']}';
-  final evaSoket = "raspberrypi.local:10001";
-  final projectDir = Directory.current.path;
+  String ideName = 'sofkip.ide.${Platform.environment['USER']}';
+  String evaSoket = "raspberrypi.local:10001";
+  String projectDir = Directory.current.path;
   final ssh = SshConfig();
 }
