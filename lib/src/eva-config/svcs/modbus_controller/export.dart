f829527d5416fa50608e7d6f2099b64853b7dabf
Andrei Losev <losev_andrei@bk.ru>
2025-07-14 17:54:05 +0300

    modbus config pull update type

--- lib/src/eva-config/svcs/modbus_controller/export.dart
+++ lib/src/eva-config/svcs/modbus_controller/export.dart
@@ -4,3 +4,4 @@ export 'package:eva_connector/src/eva-config/svcs/modbus_controller/modbus_confi
 export 'package:eva_connector/src/eva-config/svcs/modbus_controller/action_map_item.dart';
 export 'package:eva_connector/src/eva-config/svcs/modbus_controller/modbus_register.dart';
 export 'package:eva_connector/src/eva-config/svcs/modbus_controller/modbus_controller.dart';
+export 'package:eva_connector/src/eva-config/svcs/modbus_controller/pull_item.dart';
