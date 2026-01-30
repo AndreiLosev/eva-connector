6077e21959ef4d3d91cbb52a736b606780291fbb
Andrei Losev <losev_andrei@bk.ru>
2026-01-30 22:30:28 +0300

    fix python-macro

--- lib/src/eva-config/svcs/py_macros/py_macros_config.dart
+++ lib/src/eva-config/svcs/py_macros/py_macros_config.dart
@@ -3,23 +3,26 @@ import 'package:eva_connector/src/eva-config/svcs/isvc_config.dart';
 class PyMacrosConfig extends ISvcConfig {
   String? lockerSvc;
   String? mailerSvc;
+  String? alarmSvc;
   String macroDir = 'xc/py';
   Map<String, Object?> cvars = {};
 
   @override
   void loadFromMap(Map map) {
-    lockerSvc = map['lockerSvc'];
-    mailerSvc = map['mailerSvc'];
-    macroDir = map['macroDir'] ?? macroDir;
+    lockerSvc = map['locker_svc'];
+    mailerSvc = map['mailer_svc'];
+    alarmSvc = map['alarm_svc'];
+    macroDir = map['macro_dir'] ?? macroDir;
     cvars = Map<String, Object?>.from(map['cvars'] ?? {});
   }
 
   @override
   Map<String, dynamic> toMap() {
     return {
-      if (lockerSvc != null) 'lockerSvc': lockerSvc,
-      if (mailerSvc != null) 'mailerSvc': mailerSvc,
-      'macroDir': macroDir,
+      if (lockerSvc != null) 'locker_svc': lockerSvc,
+      if (mailerSvc != null) 'mailer_svc': mailerSvc,
+      if (alarmSvc != null) 'alarm_svc': alarmSvc,
+      'macro_dir': macroDir,
       'cvars': cvars,
     };
   }
--- test/eva_config/py_macros_config_test.dart
+++ test/eva_config/py_macros_config_test.dart
@@ -7,15 +7,17 @@ void main() {
     test('loadFromMap should set properties correctly', () {
       final config = PyMacrosConfig();
       final map = {
-        'lockerSvc': 'locker',
-        'mailerSvc': 'mailer',
-        'macroDir': 'new_macro_dir',
+        'locker_svc': 'locker',
+        'mailer_svc': 'mailer',
+        'alarm_svc': 'alarm',
+        'macro_dir': 'new_macro_dir',
         'cvars': {'key': 'value'}
       };
       config.loadFromMap(map);
 
       expect(config.lockerSvc, 'locker');
       expect(config.mailerSvc, 'mailer');
+      expect(config.alarmSvc, 'alarm');
       expect(config.macroDir, 'new_macro_dir');
       expect(config.cvars, {'key': 'value'});
     });
@@ -24,14 +26,30 @@ void main() {
       final config = PyMacrosConfig();
       config.lockerSvc = 'locker';
       config.mailerSvc = 'mailer';
+      config.alarmSvc = 'alarm';
       config.macroDir = 'macro_dir';
       config.cvars = {'key': 'value'};
 
       final map = config.toMap();
 
-      expect(map['lockerSvc'], 'locker');
-      expect(map['mailerSvc'], 'mailer');
-      expect(map['macroDir'], 'macro_dir');
+      expect(map['locker_svc'], 'locker');
+      expect(map['mailer_svc'], 'mailer');
+      expect(map['alarm_svc'], 'alarm');
+      expect(map['macro_dir'], 'macro_dir');
+      expect(map['cvars'], {'key': 'value'});
+    });
+
+    test('toMap should not include null optional fields', () {
+      final config = PyMacrosConfig();
+      config.macroDir = 'macro_dir';
+      config.cvars = {'key': 'value'};
+
+      final map = config.toMap();
+
+      expect(map.containsKey('locker_svc'), false);
+      expect(map.containsKey('mailer_svc'), false);
+      expect(map.containsKey('alarm_svc'), false);
+      expect(map['macro_dir'], 'macro_dir');
       expect(map['cvars'], {'key': 'value'});
     });
   });
