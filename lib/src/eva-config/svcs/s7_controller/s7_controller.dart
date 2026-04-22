f716e36cf96ca9745f782a43e9e5a0595a8f84a4
Andrei Losev <losev_andrei@bk.ru>
2026-04-22 11:50:34 +0300

    fix s7 controller

--- lib/src/eva-config/svcs/s7_controller/s7_controller.dart
+++ lib/src/eva-config/svcs/s7_controller/s7_controller.dart
@@ -2,7 +2,7 @@ import 'package:eva_connector/src/eva-config/svcs/base_svc.dart';
 import 'package:eva_connector/src/eva-config/svcs/s7_controller/s7_config.dart';
 
 class S7Controller extends BaseSvc<S7Config> {
-  static const svcCommand = "/opt/eva/svc/eva-s7-controller";
+  static const svcCommand = "/opt/eva4/svc/eva-s7-controller";
 
   S7Controller(String id) : super(id, S7Controller.svcCommand, S7Config());
 }
