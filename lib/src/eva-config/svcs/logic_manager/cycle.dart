d11c45321b35a48295acf1427079921ea2783763
Andrei Losev <losev_andrei@bk.ru>
2026-04-29 09:15:17 +0300

    rpcClient action and run fix

--- lib/src/eva-config/svcs/logic_manager/cycle.dart
+++ lib/src/eva-config/svcs/logic_manager/cycle.dart
@@ -1,11 +1,11 @@
 class Cycle {
   String id;
   bool autoStart = false;
-  num interval = 0;
+  double interval = 5;
   String run;
-  List<dynamic> args = [];
-  Map<String, dynamic> kwargs = {};
-  String onError = '';
+  List<dynamic>? args;
+  Map<String, dynamic>? kwargs;
+  String? onError;
 
   Cycle(this.id, this.run);
 
@@ -15,16 +15,16 @@ class Cycle {
       'auto_start': autoStart,
       'interval': interval,
       'run': run,
-      'args': args,
-      'kwargs': kwargs,
-      if (onError.isNotEmpty) 'on_error': onError,
+      'args': ?args,
+      'kwargs': ?kwargs,
+      'on_error': ?onError,
     };
   }
 
   void loadFromMap(Map<String, dynamic> map) {
     id = map['id'] as String? ?? '';
     autoStart = map['auto_start'] as bool? ?? false;
-    interval = map['interval'] as num? ?? 0;
+    interval = map['interval'] as double? ?? 0;
     run = map['run'] as String? ?? '';
     args = map['args'] as List<dynamic>? ?? [];
     kwargs = map['kwargs'] as Map<String, dynamic>? ?? {};
